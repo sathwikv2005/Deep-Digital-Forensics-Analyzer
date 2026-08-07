97920b9f339872486c7b8464611abf1bda9905b6
sathwikv2005 <sathwikv2005@gmail.com>
2026-08-07 15:14:13 +0530

    add event collector

--- collector/collectors/eventLogCollector.cpp
+++ collector/collectors/eventLogCollector.cpp
@@ -1,9 +1,51 @@
-#include "EventLogCollector.h"
+#include "eventLogCollector.h"
+
+#include <windows.h>
+#include <winevt.h>
 
 #include <iostream>
+#include <vector>
+
+#pragma comment(lib, "wevtapi.lib")
 
 bool EventLogCollector::collect() {
-    std::cout << "Collecting Windows Event Logs...\n";
+    const wchar_t* channel = L"System";
 
+    EVT_HANDLE query = EvtQuery(nullptr, channel, L"*",
+                                EvtQueryChannelPath | EvtQueryForwardDirection);
+
+    if (!query) {
+        std::cerr << "EvtQuery failed: " << GetLastError() << '\n';
+        return false;
+    }
+
+    EVT_HANDLE events[16];
+    DWORD returned = 0;
+
+    while (EvtNext(query, 16, events, INFINITE, 0, &returned)) {
+        for (DWORD i = 0; i < returned; i++) {
+            DWORD bufferUsed = 0;
+            DWORD propertyCount = 0;
+
+            EvtRender(nullptr, events[i], EvtRenderEventXml, 0, nullptr,
+                      &bufferUsed, &propertyCount);
+
+            if (GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+                EvtClose(events[i]);
+                continue;
+            }
+
+            std::vector<wchar_t> buffer(bufferUsed / sizeof(wchar_t));
+
+            if (EvtRender(nullptr, events[i], EvtRenderEventXml, bufferUsed,
+                          buffer.data(), &bufferUsed, &propertyCount)) {
+                std::wcout << buffer.data() << L"\n\n";
+            }
+
+            EvtClose(events[i]);
+        }
+    }
+
+    EvtClose(query);
     return true;
 }
