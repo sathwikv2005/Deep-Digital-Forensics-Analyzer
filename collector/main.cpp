ab3d454529c778753945b727eb6db18dd20a3b39
sathwikv2005 <sathwikv2005@gmail.com>
2026-08-09 18:05:48 +0530

    add eventCollector

--- collector/main.cpp
+++ collector/main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <vector>
 
 #include "collectors/EventLogCollector.h"
 
 int main() {
     EventLogCollector eventCollector;
 
-    if (eventCollector.collect()) {
-        std::cout << "Success\n";
-    } else {
-        std::cout << "Failed\n";
+    std::vector<Evidence> evidence = eventCollector.collect();
+
+    std::cout << "Collected " << evidence.size() << " events\n\n";
+
+    for (const auto& event : evidence) {
+        std::cout << "Source: " << event.source << '\n';
+        std::cout << "Timestamp: " << event.timestamp << '\n';
+        std::cout << "Category: " << event.category << '\n';
+        std::cout << "Description:\n" << event.description << '\n';
+        std::cout << "----------------------------------------\n";
     }
 
     return 0;
